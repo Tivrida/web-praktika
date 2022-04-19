f705d7e776a3ef564a12e1dcf3565d62d66ee5bb
Tivrida <75439110+Tivrida@users.noreply.github.com>
2022-04-19 14:50:20 +0300

    Add files via upload
    "create-react-app" (praktica - 6)

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import { FuncComponent } from "./FuncComponent";
+import { ClassComponent} from "./ClassComponent";
+function App() {
+  return (
+    <div className="App">
+      <center>
+        <FuncComponent/>
+        <ClassComponent/>
+      </center>
+    </div>
+  );
+}
+
+export default App;
