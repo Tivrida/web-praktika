3957deb6e56f2261ef5231e9f91fe8a3b25178a4
Tivrida <75439110+Tivrida@users.noreply.github.com>
2022-05-07 14:08:58 +0300

    Add files via upload
    Добавляю файлы по ESLint и Prettirer.

--- App.js
+++ App.js
@@ -1,25 +1,25 @@
+import logo from './logo.svg';
 import './App.css';
-import Frame from '../Frame/Frame'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import styles from './App.module.css'
-import Navbar from '../Navbar/Navbar';
-import Home from '../Home/Home';
-import Cart from '../Cart/Cart.js';
-import Footer from '../Footer/Footer.js'
 
 function App() {
-    return (
-        <div id='app'>
-            <Frame text={'Frame'}/>
-            <Router>
-                <div className={styles.app}><Navbar /><div>
-                        <Switch>
-                            <Route exact path="/"><Home /></Route>
-                            <Route path="/cart"><Cart /></Route>
-                        </Switch>
-                    </div><Footer /></div>
-                )
-            </Router>
-            )
-            }
-            export default App;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- App.test.js
+++ App.test.js
@@ -1,51 +1,8 @@
-import largeasteroid from "../assets/largeasteroid.svg";
-import mediumasteroid from "../assets/mediumasteroid.svg";
-import smallasteroid from "../assets/smallasteroid.svg";
-import styles from "../CardsArray/CardsArray.module.css";
-import dino from "../assets/dino.svg";
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-const Tekk = ({props}) => {
-
- // console.log("Tekk");
-  console.log(props);
-
-  function handleSubmit(e) {
-    e.preventDefault();
-    console.log('submit.');
-  }
-  let asteroidsrc;
-    if (props.size > 450) asteroidsrc = largeasteroid;
-      else if (props.size > 99) asteroidsrc = mediumasteroid;
-        else asteroidsrc = smallasteroid;
-  let tekkstyle;
-     if (props.isDangerous) tekkstyle = styles.dangerousCard;
-      else tekkstyle = styles.harmlessCard;
-  return (
-      <div className={tekkstyle}>
-        <div className={styles.leftblock}>
-          <img className={styles.asteroid} src={asteroidsrc}></img>
-          <img className={styles.dino} src={dino}></img>
-        </div>
-        <div className={styles.centerblock}>
-          <div className={styles.name}>{props.name}</div>
-          <div className={styles.data}>
-            <div>Размер объекта: {props.size} в метрах </div>
-            <div>Расстояние от его расположения: {props.distance} в километрах</div>
-            <div>Дата: {props.date}(на амереканский лад)</div>
-          </div>
-        </div>
-        <div className={styles.rightblock}>
-          <div>
-            <div>Оценка:</div>
-                {(props.isDangerous) ?
-                <div className={styles.grade}>Опасность</div> :
-                <div className={styles.grade}>Нет опасности</div>}
-          </div>
-          <div>
-            <form onSubmit={handleSubmit}><button className={styles.submit} type="submit">Уничтожить</button></form>
-          </div>
-        </div>
-      </div>
-  )
-}
-export default Tekk;
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
